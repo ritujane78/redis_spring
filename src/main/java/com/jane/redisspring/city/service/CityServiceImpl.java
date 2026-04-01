fd533dedf957d737b6476388fb4007303e93348e
Ritu Bafna <ritujane78@gmail.com>
2026-04-01 16:40:28 +0545

    give TTL to object fields

--- src/main/java/com/jane/redisspring/city/service/CityServiceImpl.java
+++ src/main/java/com/jane/redisspring/city/service/CityServiceImpl.java
@@ -1,15 +1,14 @@
 package com.jane.redisspring.city.service;
 
 import com.jane.redisspring.city.dto.City;
-import org.redisson.RedissonReactive;
-import org.redisson.api.RMapReactive;
-import org.redisson.api.RedissonClient;
+import org.redisson.api.RMapCacheReactive;
 import org.redisson.api.RedissonReactiveClient;
 import org.redisson.codec.TypedJsonJacksonCodec;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
+
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class CityServiceImpl implements CityService {
@@ -17,16 +16,16 @@ public class CityServiceImpl implements CityService {
     @Autowired
     private CityClient cityClient;
 
-    private RMapReactive<String, City> cityRMap;
+    private RMapCacheReactive<String, City> cityRMap;
 
     public CityServiceImpl(RedissonReactiveClient redissonClient) {
-        this.cityRMap = redissonClient.getMap("city", new TypedJsonJacksonCodec(City.class));
+        this.cityRMap = redissonClient.getMapCache("city", new TypedJsonJacksonCodec(City.class));
     }
     @Override
     public Mono<City> getCity(String zip) {
         return this.cityRMap.get(zip)
                 .switchIfEmpty(this.cityClient.getCity(zip)
-                        .flatMap(c -> this.cityRMap.fastPut(zip, c).thenReturn(c))
+                        .flatMap(c -> this.cityRMap.fastPut(zip, c, 10, TimeUnit.SECONDS        ).thenReturn(c))
                 );
     }
 }
