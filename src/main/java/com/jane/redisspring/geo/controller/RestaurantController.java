837a349d87c92e0fd98d4a7a4ebfb765199919f5
Ritu Bafna <ritujane78@gmail.com>
2026-04-06 19:33:19 +0545

    üse openstreetmaps instead of google maps

--- src/main/java/com/jane/redisspring/geo/controller/RestaurantController.java
+++ src/main/java/com/jane/redisspring/geo/controller/RestaurantController.java
@@ -3,12 +3,10 @@ package com.jane.redisspring.geo.controller;
 import com.jane.redisspring.geo.dto.Restaurant;
 import com.jane.redisspring.geo.service.RestaurantLocatorService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 
+@CrossOrigin(origins = "http://localhost:63342")
 @RestController
 @RequestMapping("/geo")
 public class RestaurantController {
